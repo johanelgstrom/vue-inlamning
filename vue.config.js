2e40a57aac5d939b7d235f1aab1280ee45ec8d4d
Johan Elgström <johan.elgstrom@gmail.com>
2022-08-28 22:37:56 +0200

    Revert "vue config change number 201"
    This reverts commit a3e983a05790def9a52739dde178a61ce8b3bd72.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+<<<<<<< HEAD
   publicPath: "/",
+=======
+  publicPath: "/project name/",
+>>>>>>> parent of a3e983a (vue config change number 201)
 });
