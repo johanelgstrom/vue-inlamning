1887015b9e736346c5513796412222237a2fbc16
Johan Elgström <johan.elgstrom@gmail.com>
2022-08-28 22:40:58 +0200

    reverted vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  publicPath: "/",
-=======
-  publicPath: "/project name/",
->>>>>>> parent of a3e983a (vue config change number 201)
-=======
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-inlamning/" : "/",
->>>>>>> parent of 8177a5a (vue config change number 200)
-=======
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-inlamning/" : "",
->>>>>>> parent of 24bec37 (fsdfsdf)
 });
